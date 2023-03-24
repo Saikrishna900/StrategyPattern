f91cd2b780c9a63e3ab29d8ff4625437e66e7f6e
Saikrishna <vadlasaikrishna900@gmail.com>
2023-03-24 09:32:18 +0530

    changes made

--- Context.java
+++ Context.java
@@ -2,7 +2,11 @@ package Strategy;
 
 public class Context {
     private CalculationStrategy sta;
-    Context(CalculationStrategy sta) {
+    Context(CalculationStrategy sta)
+    {
         this.sta = sta;
+    }
+    public int  doCal(int a,int b){
+        return sta.performCalculation(a,b);
     }
 }
--- StrategyPattern.java
+++ StrategyPattern.java
@@ -2,8 +2,15 @@ package Strategy;
 
 public class StrategyPattern {
     public static void main(String[] args) {
-        CalculationStrategy cs = new Addition();
-        System.out.println(cs.performCalculation(2,5));
+//        CalculationStrategy cs = new Addition();
+//        System.out.println(cs.performCalculation(2,5));
+    Context c = new Context(new Addition());
+        System.out.println(c.doCal(4,5));
 
+        c = new Context(new Subtraction());
+        System.out.println(c.doCal(2,3));
+
+        c = new Context(new Multiplication());
+        System.out.println(c.doCal(4,6));
     }
 }
